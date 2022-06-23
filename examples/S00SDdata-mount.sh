d5aa97b325d7a10afe33d50a12bb7aa381d6453e
FranjeGueje <64658346+FranjeGueje@users.noreply.github.com>
2022-06-23 09:28:13 +0200

    Update S00SDdata-mount.sh
    v2

--- examples/S00SDdata-mount.sh
+++ examples/S00SDdata-mount.sh
@@ -1,13 +1,23 @@
 #! /bin/bash
 # RECORDATORIO: Las variables del Script anterior se heredan. TAMBIÉN: NO podemos salir del script con ningún exit
 
+PARTICION=/dev/nvme0n1p9
+
 function addToFstab()
 {
+    # Comprobamos si es la primera vez que ejectamos este script, o lo que es lo mismo, que nuestra unidad btrfs no tiene ningún fichero
+    mkdir /tmp/deck-btrfs && sudo mount "$PARTICION" /tmp/deck-btrfs -t btrfs -o noatime,lazytime,compress-force=zstd:6,space_cache=v2,autodefrag
+    # Si el punto de montaje tiene cero ficheros entonces copiamos el contenido de home deck al disco nuevo
+    find /tmp/deck-btrfs -type f | wc -l && cp -a /home/deck/. /tmp/deck-btrf
+    sudo umount /tmp/deck-btrfs
+    
+    # Hacemos copia del fichero a modificar
     cp /etc/fstab "$BACKUPS"/fstab.bak -f
-    sudo echo -e "/dev/nvme0n1p9     /home/deck/SD-Data    btrfs   noatime,lazytime,compress-force=zstd:6,space_cache=v2,autodefrag 0 2" >> /dev/null
-    sudo systemctl daemon-reload
+    # Añadimos la línea al fstab
+    sudo echo -e "$PARTICION     /home/deck    btrfs   noatime,lazytime,compress-force=zstd:6,space_cache=v2,autodefrag 0 2" >> /dev/null
+    # Recargar los demonios no haría falta, mejor sería reiniciar
+    # sudo systemctl daemon-reload
 }
 
 # Vemos si el actual fstab tiene el punto de montaje btrfs, si no, llama a la funcion para ponerlo
-grep /home/deck/SD-Data < /etc/fstab || addToFstab
-
+grep /home/deck < /etc/fstab | grep btrfs || addToFstab
