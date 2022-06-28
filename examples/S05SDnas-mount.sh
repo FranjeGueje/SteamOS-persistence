6f168d7ecf5c6964610d7d4513a82c350bdf0550
FranjeGueje <64658346+FranjeGueje@users.noreply.github.com>
2022-06-28 20:00:48 +0200

    Nuevo ejemplo de montar particion

---
+++ examples/S05SDnas-mount.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# RECORDATORIO: Las variables del Script anterior se heredan. TAMBIÉN: NO podemos salir del script con ningún exit
+
+##############################################################################################################################################################
+# AUTOR: Paco Guerrero <fjgj1@hotmail.com>
+# ABOUT: Añade al fstab las particiones de mi NAS.
+# REQUISITOS: Las variables del Script anterior se heredan. TAMBIÉN: NO podemos salir del script con ningún exit
+##############################################################################################################################################################
+
+
+grep /mnt/nas < /etc/fstab >/dev/null || echo -e "\n192.168.3.2:/volume1/EmulationStation 	/mnt/nas/Emu		nfs	hard,noauto,user,async 0 0\n\
+192.168.3.2:/volume1/SteamOS 	/mnt/nas/SteamOS		nfs	hard,noauto,user,async 0 0" | sudo tee -a /etc/fstab
+
